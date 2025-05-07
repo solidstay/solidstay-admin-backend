f891896117d58cb56ed3da8ca3cc0be6f21a17cf
Bernoulli Mukuna <mmbernoulli@gmail.com>
2025-05-07 13:18:40 +0100

    modified url

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const userService = require('../services/userService');
-const emailService = require('../services/emailService');
-const templateUtils = require('../utils/templateUtils');
+const path = require("path");
+const userService = require("../services/userService");
+const emailService = require("../services/emailService");
+const templateUtils = require("../utils/templateUtils");
 
 const Register = async (req, res, next) => {
   try {
@@ -10,7 +10,7 @@ const Register = async (req, res, next) => {
     const user = await userService.createUser(data, userType);
     res.status(201).json({ message: "User registered successfully!" });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
@@ -18,10 +18,10 @@ const Login = async (req, res, next) => {
   try {
     const data = { ...req.body };
     const { accessToken, refreshToken } = await userService.loginUser(data);
-    res.cookie('refreshToken', refreshToken, {
+    res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: 'Strict'
+      sameSite: "Strict",
     });
     res.status(200).json({ token: accessToken });
   } catch (error) {
@@ -33,17 +33,26 @@ const ForgotPassword = async (req, res, next) => {
   try {
     const { email } = req.body;
     const { user, resetToken } = await userService.createResetToken(email);
-    const CLIENT_URL = req.get('origin');
+    const CLIENT_URL = req.get("origin");
     const resetLink = `${CLIENT_URL}/reset-password?token=${resetToken}`;
-    const templatePath = path.join(__dirname, '../templates/resetPasswordEmailTemplate.hbs');
+    const templatePath = path.join(
+      __dirname,
+      "../templates/resetPasswordEmailTemplate.hbs"
+    );
     const data = {
-      companyLogo: "http://localhost:5777/static/images/logo.png",
+      companyLogo:
+        "http://api.solidstayproperties.co.uk/static/images/logo.png",
       userName: user.name,
-      resetLink
+      resetLink,
     };
     const htmlContent = await templateUtils.generateHTML(data, templatePath);
-    await emailService.sendEmail(user.email, 'Password Reset Request', null, htmlContent);
-    res.status(200).json({ message: 'Reset password link sent!' });
+    await emailService.sendEmail(
+      user.email,
+      "Password Reset Request",
+      null,
+      htmlContent
+    );
+    res.status(200).json({ message: "Reset password link sent!" });
   } catch (error) {
     next(error);
   }
@@ -62,11 +71,13 @@ const ResetPassword = async (req, res, next) => {
 const RefreshToken = async (req, res, next) => {
   try {
     const refreshToken = req.cookies?.refreshToken;
-    const { newAccessToken, newRefreshToken } = await userService.refreshToken(refreshToken);
-    res.cookie('refreshToken', newRefreshToken, {
+    const { newAccessToken, newRefreshToken } = await userService.refreshToken(
+      refreshToken
+    );
+    res.cookie("refreshToken", newRefreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: 'Strict'
+      sameSite: "Strict",
     });
     res.status(200).json({ token: newAccessToken });
   } catch (error) {
@@ -80,12 +91,12 @@ const Logout = async (req, res, next) => {
     if (refreshToken) {
       await userService.logoutUser(refreshToken);
     }
-    res.clearCookie('refreshToken', {
+    res.clearCookie("refreshToken", {
       httpOnly: true,
       secure: true,
-      sameSite: 'Strict'
+      sameSite: "Strict",
     });
-    res.status(200).json({ message: 'Logged out successfully' });
+    res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     next(error);
   }
@@ -128,7 +139,12 @@ const SearchEmployees = async (req, res, next) => {
     const { pageIndex, limit, searchQuery } = req.query;
     const parsedPageIndex = parseInt(pageIndex);
     const parsedLimit = parseInt(limit);
-    const result = await userService.searchUsers(parsedPageIndex, parsedLimit, searchQuery, "employee");
+    const result = await userService.searchUsers(
+      parsedPageIndex,
+      parsedLimit,
+      searchQuery,
+      "employee"
+    );
     res.status(200).json({ result });
   } catch (error) {
     next(error);
@@ -140,10 +156,15 @@ const SearchUsers = async (req, res, next) => {
     const { pageIndex, limit, searchQuery, status } = req.query;
     const parsedPageIndex = parseInt(pageIndex);
     const parsedLimit = parseInt(limit);
-    const result = await userService.searchUsers(parsedPageIndex, parsedLimit, searchQuery, "user");
+    const result = await userService.searchUsers(
+      parsedPageIndex,
+      parsedLimit,
+      searchQuery,
+      "user"
+    );
     if (status && result.users && result.users.length > 0) {
-      result.users = result.users.filter(user => {
-        const lowerCaseArr = user.status.map(status => status.toLowerCase());
+      result.users = result.users.filter((user) => {
+        const lowerCaseArr = user.status.map((status) => status.toLowerCase());
         const lowerCaseStatus = status.toLowerCase();
         return lowerCaseArr.includes(lowerCaseStatus);
       });
@@ -157,22 +178,22 @@ const SearchUsers = async (req, res, next) => {
 const CreateEmployee = async (req, res, next) => {
   try {
     const data = { ...req.body };
-    data.role = 'employee';
+    data.role = "employee";
     const user = await userService.createUser(data);
     res.status(201).json({ message: "Employee created successfully!" });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
 const CreateUser = async (req, res, next) => {
   try {
     const data = { ...req.body };
-    data.role = 'user';
+    data.role = "user";
     const user = await userService.createUser(data);
     res.status(201).json({ message: "User created successfully!" });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
@@ -181,7 +202,7 @@ const UpdateEmployee = async (req, res, next) => {
     const { userId } = req.params;
     const data = { ...req.body };
     await userService.updateUser(req.config.stripe, userId, data, "employee");
-    res.status(200).json({ message: 'Employee info updated successfully!' });
+    res.status(200).json({ message: "Employee info updated successfully!" });
   } catch (error) {
     next(error);
   }
@@ -192,7 +213,7 @@ const UpdateUser = async (req, res, next) => {
     const { userId } = req.params;
     const data = { ...req.body };
     await userService.updateUser(req.config.stripe, userId, data, "user");
-    res.status(200).json({ message: 'User info updated successfully!' });
+    res.status(200).json({ message: "User info updated successfully!" });
   } catch (error) {
     next(error);
   }
@@ -202,7 +223,7 @@ const DeleteEmployee = async (req, res, next) => {
   try {
     const { userId } = req.params;
     await userService.deleteUser(userId, "employee");
-    res.status(200).json({ message: 'Employee deleted successfully!' });
+    res.status(200).json({ message: "Employee deleted successfully!" });
   } catch (error) {
     next(error);
   }
@@ -212,7 +233,7 @@ const DeleteUser = async (req, res, next) => {
   try {
     const { userId } = req.params;
     await userService.deleteUser(userId, "user");
-    res.status(200).json({ message: 'User deleted successfully!' });
+    res.status(200).json({ message: "User deleted successfully!" });
   } catch (error) {
     next(error);
   }
@@ -235,5 +256,5 @@ module.exports = {
   UpdateEmployee,
   UpdateUser,
   DeleteEmployee,
-  DeleteUser
+  DeleteUser,
 };
